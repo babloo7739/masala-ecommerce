9c8f68913da58cd1bcaf5b6698e04de4611b208d
Babloo Yadav <babuparsauni@gmail.com>
2026-04-24 12:54:00 +0530

    fix baseURL remove /api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 //const API = axios.create({ baseURL: "http://localhost:5000/api" });
 
-const API = axios.create({ baseURL: "https://masala-backend-tfgf.onrender.com/api" });
+const API = axios.create({ baseURL: "https://masala-backend-tfgf.onrender.com/api/auth/login" });
 
 
 
