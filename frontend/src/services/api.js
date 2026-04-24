4a206baf0f72fc243e633fbc04cba8c883ea7166
Babloo Yadav <babuparsauni@gmail.com>
2026-04-24 14:02:29 +0530

    fix api baseURL correct

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,12 +2,9 @@ import axios from "axios";
 
 //const API = axios.create({ baseURL: "http://localhost:5000/api" });
 
-// const API = axios.create({ baseURL: "https://masala-backend-tfgf.onrender.com/api/auth/login" });
+const API = axios.create({ baseURL: "https://masala-backend-tfgf.onrender.com/api" });
 
 
-const API = axios.create({
-  baseURL: "https://masala-backend-tfgf.onrender.com"
-});
 
 API.interceptors.request.use((req) => {
   const userInfo = localStorage.getItem("userInfo");
