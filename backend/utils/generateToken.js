86fccbd98fb3ab5fd78a6b0a9ceb084d0fb75a62
Babloo Yadav <babuparsauni@gmail.com>
2026-04-24 11:26:40 +0530

    updated project changes

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,10 +1,3 @@
-// const jwt = require("jsonwebtoken");
-
-// const generateToken = (userId) => {
-//   return jwt.sign({ id: userId }, process.env.JWT_SECRET, { expiresIn: "30d" });
-// };
-
-// module.exports = generateToken;
 
 const jwt = require("jsonwebtoken");
 
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000/api" });
+//const API = axios.create({ baseURL: "http://localhost:5000/api" });
+
+const API = axios.create({ baseURL: "https://masala-backend-tfgf.onrender.com/api" });
+
+
 
 API.interceptors.request.use((req) => {
   const userInfo = localStorage.getItem("userInfo");
